c4facc9c2ffa281a90085f4d7d6f3051901abf17
Evin Le <imevinle@gmail.com>
2025-10-26 00:56:21 +1000

    style(Menu): styled MenuItem dimensions and pressed state

--- arix-front/src/components/Menu/MenuItem.tsx
+++ arix-front/src/components/Menu/MenuItem.tsx
@@ -7,8 +7,11 @@ type MenuItemProps = {
 const MenuItem: React.FC<MenuItemProps> = ({ config }) => {
   return (
     <div
-      className="bg-blue-900 rounded-md cursor-pointer active:hover:bg-blue-400 
-      active:hover:scale-95 active:hover:translate-0.5 px-2 py-1 transition"
+      className="bg-blue-900 rounded-md cursor-pointer px-2 py-1 transition min-w-1/3 
+      flex justify-center items-center select-none
+    active:hover:bg-gray-900 focus active:hover:border-white active:hover:border-2 
+      active:hover:scale-95 active:hover:translate-0.5
+      "
       onClick={config.onClick}
     >
       {config.label}
