31559eb5a32abb0b4359eaf17e862cbcb133a199
Evin Le <imevinle@gmail.com>
2025-10-26 01:05:55 +1000

    style(Menu): changed border to outline for smoother button press

--- arix-front/src/components/Menu/MenuItem.tsx
+++ arix-front/src/components/Menu/MenuItem.tsx
@@ -7,9 +7,9 @@ type MenuItemProps = {
 const MenuItem: React.FC<MenuItemProps> = ({ config }) => {
   return (
     <div
-      className="bg-blue-900 rounded-md cursor-pointer px-2 py-1 transition min-w-1/3 
+      className="bg-blue-900 rounded-xl cursor-pointer px-2 py-1 transition min-w-1/3 
       flex justify-center items-center select-none
-    active:hover:bg-gray-900 focus active:hover:border-white active:hover:border-2 
+    active:hover:bg-gray-900 focus active:hover:outline-white active:hover:outline-1 
       active:hover:scale-95 active:hover:translate-0.5
       "
       onClick={config.onClick}
