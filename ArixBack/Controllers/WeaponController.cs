633208626157a88708abf2c971c52a40d095cf1f
Evin Le <imevinle@gmail.com>
2025-10-31 01:12:36 +1000

    chore(WeaponsController): renamed private fields

--- ArixBack/Controllers/WeaponController.cs
+++ ArixBack/Controllers/WeaponController.cs
@@ -13,18 +13,18 @@ namespace ArixBack.Controllers
     [Route("Weapons")]
     public class WeaponController(WeaponService weaponService) : ControllerBase
     {
-        private WeaponService _weaponService => weaponService;
+        private WeaponService _WeaponService => weaponService;
 
         [HttpGet("GetAllWeapons")]
         public async Task<ActionResult<List<Weapon>>> GetAllWeapons()
         {
-            return Ok(await _weaponService.GetWeapons());
+            return Ok(await _WeaponService.GetWeapons());
         }
 
         [HttpGet("GetWeapon")]
         public async Task<ActionResult<Weapon>> GetWeapon(string id)
         {
-            var wep = await _weaponService.GetWeapon(id);
+            var wep = await _WeaponService.GetWeapon(id);
 
             if (wep == null) return NotFound($"No weapon with ID: {id}");
 
@@ -36,9 +36,8 @@ namespace ArixBack.Controllers
         {
             if (newWeapon == null) return BadRequest("No Weapon provided");
 
-            newWeapon.Id = null;
-            await _weaponService.CreateWeapon(newWeapon);
-            return Ok();     
+            await _WeaponService.CreateWeapon(newWeapon);
+            return Ok();
         }
         [HttpPost("UpdateWeapon")]
         public async Task<ActionResult> UpdateWeapon([FromBody] Weapon wep)
@@ -46,22 +45,22 @@ namespace ArixBack.Controllers
 
             if (wep == null) return BadRequest("No Weapon provided");
 
-            var updatedRow = await _weaponService.UpdateWeapon(wep.Id,wep);
+            var updatedRow = await _WeaponService.UpdateWeapon(wep.Id, wep);
 
             if (updatedRow == false) return BadRequest($"Could not update {wep}");
 
-            return NoContent();     
+            return NoContent();
         }
         [HttpPost("RemoveWeapon")]
         public async Task<ActionResult> RemoveWeapon(string id)
         {
-            var deletedRow = await _weaponService.RemoveWeapon(id);
+            var deletedRow = await _WeaponService.RemoveWeapon(id);
 
             if (deletedRow.DeletedCount == 0) return BadRequest($"Could not delete weapon with ID: {id}");
 
             return Ok(deletedRow.ToJson());
         }
- 
+
 
     }
 }
