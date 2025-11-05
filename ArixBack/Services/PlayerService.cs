6952e82d7a7760652ab2b2adf9f9ce11b95d277b
gian <gianquiros0@gmail.com>
2025-11-05 09:02:07 +0800

    chore(PlayerService): adjusted syntax

--- ArixBack/Services/PlayerService.cs
+++ ArixBack/Services/PlayerService.cs
@@ -16,16 +16,21 @@ namespace ArixBack.Services
          public async Task<List<Player>> GetPlayers() =>
             await _db.GetPlayerCollection().Find(_ => true).ToListAsync();
 
-        public async Task<Player?> GetPlayer(int id) =>
-            await _db.GetPlayerCollection().Find(x => x.id == id).FirstOrDefaultAsync();
+        public async Task<Player?> GetPlayer(string id) =>
+            await _db.GetPlayerCollection().Find(GetPlayerByIdFilter(id)).FirstOrDefaultAsync();
 
         public async Task CreatePlayer(Player newPlayer) =>
             await _db.GetPlayerCollection().InsertOneAsync(newPlayer);
 
-        public async Task UpdatePlayer(int id, Player updatedplayer) =>
-            await _db.GetPlayerCollection().ReplaceOneAsync(x => x.id == id, updatedplayer);
+        public async Task UpdatePlayer(string id, Player updatedplayer) =>
+            await _db.GetPlayerCollection().ReplaceOneAsync(GetPlayerByIdFilter(id), updatedplayer);
 
-        public async Task RemovePlayer(int id) =>
-            await _db.GetPlayerCollection().DeleteOneAsync(x => x.id == id);
+        public async Task RemovePlayer(string id) =>
+            await _db.GetPlayerCollection().DeleteOneAsync(GetPlayerByIdFilter(id));
+
+            private static MongoDB.Driver.FilterDefinition<Player> GetPlayerByIdFilter(string id)
+        {
+            return Builders<Player>.Filter.Eq(player => player.Id, id);  
+        }
     }
 }
